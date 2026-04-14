47b067af7bdfb56ea0810c907b39ff053e6a1960
Yi Chern <pekyichern@gmail.com>
2026-04-14 16:54:45 +0800

    made changes

--- src/entity/Item.java
+++ src/entity/Item.java
@@ -1,7 +1,21 @@
 package entity;
 
-public interface Item {
-    String getName();
-    String getDescription();
-    void use(Player user);
+import entity.actions.TargetType;
+
+public abstract class Item {
+	private String name;
+	private String description;
+	private TargetType targeting;
+	
+	public Item(String name, String description, TargetType targeting) {
+		this.name = name;
+		this.description = description;
+		this.targeting = targeting;
+	}
+	
+	public String getName() { return name; }
+    public String getDescription() { return description; }
+    public TargetType getTargeting() { return targeting; }
+    
+    public abstract void use(Player user);
 }
--- src/entity/SmokeBomb.java
+++ src/entity/SmokeBomb.java
@@ -1,16 +1,13 @@
 package entity;
 
+import entity.actions.TargetType;
 import entity.effects.SmokeBombEffect;
 
-public class SmokeBomb implements Item {
+public class SmokeBomb extends Item {
+    public SmokeBomb() {
+		super("Smoke Bomb", "Enemy attacks deal 0 damage for 2 turns", TargetType.SELF);
+	}
 
-    @Override
-    public String getName() { return "Smoke Bomb"; }
-
-    @Override
-    public String getDescription() { return "Enemy attacks deal 0 damage for 2 turns"; }
-
-    @Override
     public void use(Player user) {
     	user.applyStatus(new SmokeBombEffect());
 //        System.out.println(user.getName() + " used Smoke Bomb! Enemy attacks deal 0 damage for 2 turns.");
